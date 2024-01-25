04b863249999d65c711fd0f0be71fc231c116f5b
Santiago Pueyo <89522040+sxdisbr@users.noreply.github.com>
2024-01-25 13:10:50 +0300

    Update delivery_service.py
    Eliminates the unnecessary else

--- delivery_service.py
+++ delivery_service.py
@@ -1,17 +1,23 @@
 from typing import List
 
+
 def transport_robots(weight: List[int], limit: int) -> int:
     """
     Calculate the minimum number of transport platforms needed to move robots.
     """
     weight.sort(reverse=True)
-    platforms = 0
-    while weight:
-        actual_weight = weight.pop(0)
-        if weight and actual_weight + weight[-1] <= limit:
-            weight.pop(0)
-        platforms += 1
+    platforms: int = 0
+    left_pointer, right_pointer = 0, len(weight) - 1
+
+    while left_pointer <= right_pointer:
+        # If two robots can fit on the platform, we move both pointers.
+        if weight[left_pointer] + weight[right_pointer] <= limit:
+            left_pointer += 1
+        # In any case, move the right pointer.
+        right_pointer -= 1
+
     return platforms
+
 
 def main():
     weight = list(map(int, input().split()))
@@ -19,5 +25,7 @@ def main():
     result = transport_robots(weight, limit)
     print(result)
 
+
 if __name__ == '__main__':
     main()
+
